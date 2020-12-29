79f93ff0dad5225413af891e007dc64f72180dc8
tom_glb <tom_glb@zt.com>
2020-12-29 23:12:01 +0800

    HttpController committo local repository

---
+++ src/main/java/com/zhouteng/springboot/springbootmonday/HttpController.java
@@ -0,0 +1,12 @@
+package com.zhouteng.springboot.springbootmonday;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HttpController {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    public static void main(String[] args) {
+
+    }
+}
